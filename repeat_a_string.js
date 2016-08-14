07b100dc9a0977492afbf7b8731fa76288ed2fc8
bacitracin <tracyctran@gmail.com>
2016-08-13 21:32:58 -0700

    add fcc repeat a string problem

---
+++ repeat_a_string.js
@@ -0,0 +1,11 @@
+// Repeat a given string (first argument) num times (second argument). 
+// Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+
+}
+
+repeatStringNumTimes("abc", 3);
+
+// repeatStringNumTimes("*", 3) should return "***".
+// repeatStringNumTimes("abc", 1) should return "abc".
