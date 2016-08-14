41c5a4163e0f14e5c09169b8f5359e0f7688e81e
bacitracin <tracyctran@gmail.com>
2016-08-13 20:45:05 -0700

    add solution using Math

--- fcc_return_largest_numbers_in_arrays.js
+++ fcc_return_largest_numbers_in_arrays.js
@@ -2,9 +2,15 @@
 // For simplicity, the provided array will contain exactly 4 sub-arrays.
 
 function largestOfFour(arr) {
-  // You can do this!
+  var largestArray = [];
   
-  return arr;
+  for (i = 0; i < arr.length; i++) { 
+    var maxOfArray = Math.max.apply(Math, arr[i]);
+    console.log()
+    largestArray.push(maxOfArray)
+  }
+  
+  return largestArray; // Should be => [5, 27, 39, 1001]
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
