3609b960b26e4cf4f85cb1edea9539f76e272a28
bacitracin <tracyctran@gmail.com>
2016-08-13 21:23:37 -0700

    add solution using substr

--- fcc_confirm_the_ending.js
+++ fcc_confirm_the_ending.js
@@ -2,9 +2,7 @@
 // the given target string (second argument, target)
 
 function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-  return str;
+  return str.substr(-target.length) === target;
 }
 
 confirmEnding("Bastian", "n");
