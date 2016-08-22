47568f1cf87c67d4ec69a69ad25bb3780355d5c2
bacitracin <tracyctran@gmail.com>
2016-08-22 11:55:17 -0700

    complete solution for ic # 26 reverse a string in place

--- interviewcake_26_reverse_a_string_in_place.rb
+++ interviewcake_26_reverse_a_string_in_place.rb
@@ -1,1 +1,19 @@
-test_string = "I am a test"
+test_string = "Tracy"
+
+# start at the ends and swap the characters, moving toward the middle
+def reverse_string_in_place(string)
+  left_counter = 0
+  right_counter = string.length - 1
+
+  while left_counter < right_counter
+
+    string[left_counter], string[right_counter] = \
+      string[right_counter], string[left_counter]
+
+      left_counter += 1
+      right_counter -= 1
+    end
+
+    return string
+
+end
