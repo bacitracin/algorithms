9be7d420b149359c5840a9a53304c36042c42103
bacitracin <tracyctran@gmail.com>
2016-08-28 22:35:02 -0700

    add add_digits_constant solution

--- ms_add_digits_constant.rb
+++ ms_add_digits_constant.rb
@@ -3,3 +3,16 @@
 # For example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. 
 # Since 2 has only one digit, return it.
 
+# Important that it's already a non-negative input
+
+def add_digits(num) # 38
+  converted_num = num.to_s.split("") # ["3", "8"]
+  num_sum = 0
+
+  converted_num.each do |num|
+    num_sum += num.to_i # don't forget the string needs to be converted 
+  end
+
+  num_sum
+
+end
