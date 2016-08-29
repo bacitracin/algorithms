1a2dcc1eab11e83fb53efc303501544c88173af8
bacitracin <tracyctran@gmail.com>
2016-08-28 21:59:48 -0700

    add my notes from my mock technical interview

--- string_in_string.rb
+++ string_in_string.rb
@@ -28,3 +28,34 @@ __A____B
 ____AB__
 ____A__B
 ______AB
+
+
+
+# p arr.each_index.select{|i| arr[i] == 'x'} # =>[0, 2, 6]
+
+def find_num_combos("ab", "ababab") 
+   search_term_array = search_term.split("") #=> ["a", "b"]
+   first_search_letter = search_term_array[0] #=> "a"
+   second_search_letter = search_term_array[1] #=> "b"
+   
+   string_to_search_array = string_to_search.split("") # => ["a", "b", "a", "b", "a", "b"]
+   
+   # brute - loop over each part of the array, check the letter
+   
+   #if the letter is the first letter we're searching for, continue on, otherwise move on to next
+   
+   full_search_term_counter = 0
+   string_to_search_array.each_with_index { |letter, index|
+      
+       if letter == first_search_letter 
+         string_to_search_array[ index+1..-1 ].each{ |letter|
+             full_search_term_counter += 1 if letter == second_search_letter
+         
+                     
+         
+         }
+
+       end
+       
+   }
+   
