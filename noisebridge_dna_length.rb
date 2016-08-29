0f19707b8dd2ff12600c5aa03ab6e1b9dfe1b916
bacitracin <tracyctran@gmail.com>
2016-08-28 22:19:21 -0700

    add solution to dna length problem

--- noisebridge_dna_length.rb
+++ noisebridge_dna_length.rb
@@ -1,7 +1,32 @@
-DNA LENGTH
+# DNA LENGTH
 
-Difficulty: Easy
+# Difficulty: Easy
 
-Question:
-Given a string, find the longest length of DNA sequence inside it, where DNA is made up of A, C, T, and G characters. For example, in the string “NZCVNASFAHOSDFHNASDAGTCGATCGKJASLSFJ", the longest DNA substring is AGTCGATCG, which is 9 characters long. You should write a function which takes in a string and outputs an integer.
+# Question:
+# Given a string, find the longest length of DNA sequence inside it, 
+# where DNA is made up of A, C, T, and G characters. 
 
+# For example, in the string “NZCVNASFAHOSDFHNASDAGTCGATCGKJASLSFJ", 
+# the longest DNA substring is AGTCGATCG, which is 9 characters long. 
+# You should write a function which takes in a string and outputs an integer.
+
+def longest_dna_sequence(string)
+  max_sequence_length = 0 # Running counter of the longest sequence 
+  current_sequence_length = 0 # Another counter counting up the current length of the DNA substring as you iterate
+
+  string_array = string.split("")
+
+  for character in string_array
+    if "agtc".include?(character)
+      current_sequence_length += 1
+      if current_sequence_length > max_sequence_length
+        max_sequence_length = current_sequence_length
+      end
+    else
+      current_sequence_length = 0 #reset
+    end
+  end
+
+  return max_sequence_length
+
+end
