b23d56886b36b9d1fc4fa07292f1a8001a7920e5
bacitracin <tracyctran@gmail.com>
2016-09-02 15:40:35 -0700

    add JS version

--- cr_find_dupes_in_array.rb
+++ cr_find_dupes_in_array.rb
@@ -1,6 +1,27 @@
+# Ruby version
+
 def find_dupes(arr)
 
   dupes_list = arr.select{|num| arr.count(num) > 1}
   dupes_list.uniq
 
 end
+
+
+# JS 
+
+function findDupes(arr)
+  var dict = {}
+  var dupes_list= [];
+
+  for (var i = 0; i < arr.length; i++){
+    var val = arr[i];
+    if (dict[val]){
+      dupes_list.push(val);
+      }
+    }
+    else {
+      dict[val] 1;
+    }
+  return dupes_list;
+}
