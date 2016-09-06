6229ecddc2a3585ee94d98a3b2c2a57697809419
bacitracin <tracyctran@gmail.com>
2016-09-06 08:53:17 -0700

    added codewars largest pair sum in array problem and answer - hint - for Ruby use inject

---
+++ codewars_largest_pair_sum_in_array.rb
@@ -0,0 +1,13 @@
+# Given an array of numbers, find the largest pair sum in the array.
+
+# For example
+# [10,14,2,23,19] should return 42 (i.e. sum of 23,19)
+# [99,2,2,23,19] should return 122 (i.e. sum of 99,23)
+
+# Input array contains minimum two elements and every element is a number.
+
+def largest_pair_sum(array)
+
+  array.max(2).inject(0) {|sum, x| sum + x}
+
+end
