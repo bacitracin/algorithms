47958c6f69210065a008b01c4e6db69d0646c5b7
bacitracin <tracyctran@gmail.com>
2016-09-19 16:21:48 -0700

    add random solution, add one solution

--- pramp_getting_different_num.rb
+++ pramp_getting_different_num.rb
@@ -2,3 +2,25 @@
 
 # Your solution should return such an integer or null if arr contains all possible integers.
 # Analyze the runtime and space complexity of your solution.
+
+# Example array [1, 500, 6, 20, 5, 8]
+# Nums are non-negative, unique, this n = 6
+# Example output 2
+
+# First approach create random numbers and compare
+
+def generate_diff_num_randomly(array)
+  random_number = -1
+
+  # Have to compare every time...
+  if array.include? random_number
+    random_number = rand(1..1000)
+  else
+    random_number
+  end
+end
+
+def generate_diff_num_add_one(array)
+  array_max = array.max
+  array_max + 1
+end
