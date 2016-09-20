97400d5f5a4088648186ede02910175bd6072f82
bacitracin <tracyctran@gmail.com>
2016-09-19 23:10:57 -0700

    add ruby solution for repeat a string

--- fcc_repeat_a_string.js
+++ fcc_repeat_a_string.js
@@ -16,3 +16,15 @@ repeatStringNumTimes("abc", 3);
 
 // repeatStringNumTimes("*", 3) should return "***".
 // repeatStringNumTimes("abc", 1) should return "abc".
+
+def repeat_string_num_times(str, num)
+
+  return_string = ""
+
+  (1..num).each do |n|
+    return_string += str
+  end
+
+  return_string
+  
+end
