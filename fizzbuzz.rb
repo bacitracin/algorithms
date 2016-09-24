48be98b1c29fac6a4a74aa345bc6a255b5f3343b
bacitracin <tracyctran@gmail.com>
2016-09-24 13:23:39 -0700

    add string solution to FizzBuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,3 +2,15 @@
 # For multiples of three print “Fizz” instead of the number 
 # and for multiples of five print “Buzz”. For numbers which are 
 # multiples of both three and five print “FizzBuzz”
+
+def fizzbuzz()
+  
+  (1..100).each do |num|
+    return_string = ""
+    return_string += "Fizz" if num % 3 == 0
+    return_string += "Buzz" if num % 5 == 0
+    return_string = num if return_string == ""
+    puts return_string
+  end
+
+end
