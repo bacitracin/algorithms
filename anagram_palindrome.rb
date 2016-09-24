2e6baf3ef2abd4a7679f8c9f0ea4588e892af613
bacitracin <tracyctran@gmail.com>
2016-09-23 19:54:56 -0700

    add anagram_palindrome, max_length_substring problems

---
+++ anagram_palindrome.rb
@@ -0,0 +1,23 @@
+# anagram_palindrome?
+#
+# Write a function which accepts a word, and returns true or false if there exists some anagram of that word that is a palindrome.
+#
+
+def anagram_palindrome?(word)
+  ## loop through the word and add to my hash, keep track of count    mentions { :c => 2, :a => 2, ....}
+  ## at the end, check that every key's value is even 
+  ## if one is odd, make sure there's only one letter that has odd value
+  
+  letter_count = Hash.new(0)
+  
+  word.each_char do |c|
+    letter_count[c] += 1
+  end
+  
+  #check that at most 1 letter has odd value
+  letter_count.values.count { |value| value.odd? } > 1? false:true
+  
+end
+
+p anagram_palindrome?("carrace") ## true
+p anagram_palindrome?("cutoo") ## false
---
+++ max_length_substring.rb
@@ -0,0 +1,16 @@
+# Given a string consisting of 'a' and 'b', and also given an
+# integer M. Return the max length substring you can generate by 
+# making at most M changes, such that all the characters of the # substring are same.
+
+# INPUTS
+# string "ab"
+# integer 2
+
+# OUTPUTS 
+# Max length substring that you can generate by making at most M changes, 
+# all of the characters of the substring are the same - 2 in this case??
+
+def max_length_substring(ab_string, integer)
+
+  #returns a number length of substring
+end
