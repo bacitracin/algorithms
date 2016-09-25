c3439722bb95b46e74cde2fa3c188c286c55055a
bacitracin <tracyctran@gmail.com>
2016-09-25 16:18:30 -0700

    update Lonely Integer problem

--- hackerrank_lonely_integer.rb
+++ hackerrank_lonely_integer.rb
@@ -5,13 +5,19 @@
 
 def  lonelyInteger( arr) 
     
-    element_count = Hash.new(0)
+  element_count = Hash.new(0)
     
-    arr.each do |element|
-        element_count[element] += 1
-    end
+  arr.each do |element|
+    element_count[element] += 1
+  end
     
-    answer_hash = element_count.select{|key| element_count[key] == 1}
-    answer_hash.keys.first
+  # answer_hash = element_count.select{|key| element_count[key] == 1}
+  # answer_hash.keys.first
+
+  a.find { |element| puts element if element_count[element]} == 1
 
 end
+
+# Hash map answer has O(n) time, O(n) space
+# Lowering space would be O(1), not using any data structures
+# Bitwise manipulation - XOR suggested
