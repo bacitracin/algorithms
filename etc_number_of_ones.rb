8c4ce900938424cb5e12ac4598b9ee1184f79027
bacitracin <tracyctran@gmail.com>
2016-09-30 16:41:02 -0700

    add number of ones problem

---
+++ etc_number_of_ones.rb
@@ -0,0 +1,15 @@
+# Number of Ones
+# Given a sorted bit array (values of 0, and 1) determine the number of 1’s in the array.
+
+# Input: Array of elements with values belong to the set S : { 0, 1 }
+# Output: Integer
+# Example
+# Input: [0, 0, 0, 1, 1, 1] =>  Output: 3
+# Input: [0, 0, 0, 0]   =>  Output: 0
+
+# Constraints
+# Time Complexity: O(logN) A linear search is not acceptable for runtime.
+# Auxiliary Space Complexity: O(1)
+
+def number_of_ones(input_array)
+end
