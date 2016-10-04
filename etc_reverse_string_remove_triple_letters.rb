6ae51fabe1355865f0c7e966d0675b1540db0dd8
bacitracin <tracyctran@gmail.com>
2016-10-04 14:08:35 -0700

    trying different approaches to reverse string remove triple letters

--- etc_reverse_string_remove_triple_letters.rb
+++ etc_reverse_string_remove_triple_letters.rb
@@ -24,3 +24,17 @@ def reverse_and_remove_triples(string) # "testbbbcd"
 
   return_string_array
 end 
+
+def reverse_remove_triples(string) # "trestbbbcd"
+
+  reverse_string_array = string.chars.reverse # ["d","c", "b", "b","b", "t","s","e","r","t"]
+
+  (0..reverse_string_array.length-3).each do |i|
+    if reverse_string_array[i] == reverse_string_array[i + 1] && reverse_string_array[i +1] == reverse_string_array[i + 2]
+      reverse_string_array.shift(3)
+    end
+  end
+
+  puts reverse_string_array.join("")
+
+end
