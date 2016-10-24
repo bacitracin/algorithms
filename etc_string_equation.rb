853df63258bf59eb0e75cfb2640419f91e78007e
bacitracin <tracyctran@gmail.com>
2016-10-24 15:20:40 -0700

    formatting longest_sequence_consecutive_numbers and add string equation calculator problem

---
+++ etc_string_equation.rb
@@ -0,0 +1,11 @@
+# Create a function that takes in a string, 
+# evaluates it as a mathematical expression 
+# and returns the correct answer. 
+# Remember the order of operations.
+
+# "6 + 3 * 4 + 2 - 2 / 2 * 10" = 10
+
+def string_calculator(str)
+
+
+end
